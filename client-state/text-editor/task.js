118cd8a9099ef0ec0ec6347f9a256d5237190a8a
Mariya5025 <139927721+Mariya5025@users.noreply.github.com>
2023-08-05 15:47:56 +0300

    Update task.js

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,13 +1,17 @@
-'use strict';
- 
-document.getElementById('editor').oninput = function (event) {
-    localStorage.textEditor ? localStorage.textEditor = JSON.stringify(event.target.value) : localStorage.setItem('textEditor', JSON.stringify(event.target.value));
-};
+let input = document.querySelector('#editor');
+window.onload = function() {
+    let inputedText = localStorage.getItem('text');
+    input.textContent = inputedText;
+}
+input.insertAdjacentHTML('afterend', '<button type="button" class= "clear" id="clear">Очистить содержимое</button>');
 
- 
-document.querySelector('div.text__clear').onclick = function () {
-    localStorage.textEditor ? localStorage.removeItem('textEditor') : false;
-    textEditor.value = '';
-};
+let button = document.getElementById('clear');
+button.addEventListener('click', e => {
+    input.value = '';    
+    e.preventDefault();
+});
 
+input.addEventListener('input', e => {
+    localStorage.setItem('text', `${e.target.value}`);
+});
  
